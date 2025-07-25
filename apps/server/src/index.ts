573a66ecd753c34b0dd27c223e5f5274b7208a92
Nathan Lopez <nathancarllopez@gmail.com>
2025-07-25 18:50:35 -0500

    Added default message for server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,10 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
+
+app.get("/", (_, res) => {
+  res.send("Hello from the backend!")
+})
 
 app.get("/server/health", (_, res) => {
   res.send("Server is healthy");
 })
 
-app.listen(PORT, () => console.log("Hello from the backend!"));
+app.listen(PORT, () => console.log(`Running on port: ${PORT}`));
